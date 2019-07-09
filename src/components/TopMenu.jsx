fd5e2cce63baab3f71cb24400afaf79e09d25beb
jakob <drusany.jakob@gmail.com>
2019-07-09 09:51:51 +0200

    stylized top bar a bit more - added links

--- src/components/TopMenu.jsx
+++ src/components/TopMenu.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import '../styles/TopMenu.scss'
 import bslogo from '../res/img/bs_power_bel_croped.png'
+import { Link } from 'react-router-dom';
 
 class TopMenu extends Component{
     render(){
@@ -8,7 +9,19 @@ class TopMenu extends Component{
             <div className="topBar">
                 <div className="innerBarWrapper">
                     <div className="innerBar">
-                        <img className="bslogo" src={bslogo} alt="Business Solutions logo"/>
+                        <div className="topBarBlock">
+                            <img className="bslogo" src={bslogo} alt="Business Solutions logo"/>
+                        </div>
+                        <div className="topBarBlock">
+                            <div className="topLinkWrapper">
+                                <Link className="topLink" to="/">Domov</Link>
+                            </div>
+                        </div>
+                        <div className="topBarBlock linkLonger">
+                            <div className="topLinkWrapper">
+                                <Link className="topLink" to="/features">Opis produkta</Link>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
