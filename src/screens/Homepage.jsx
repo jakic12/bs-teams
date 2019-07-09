4443e69c835f39da485a9fb8b4858951cf47c7a5
jakob <drusany.jakob@gmail.com>
2019-07-09 13:39:51 +0200

    removed moch from homepage

--- src/screens/Homepage.jsx
+++ src/screens/Homepage.jsx
@@ -5,15 +5,7 @@ class Homepage extends Component{
     render(){
         return (
             <div>
-                <div>
-                    <h6>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h6>
-                    <p>Nunc ac sapien vulputate odio convallis posuere nec vitae magna.
-                        In a efficitur ex, eget dictum elit. Nullam ac elit blandit, pharetra augue id, pulvinar ipsum.
-                        Maecenas vel libero odio. Mauris vehicula neque ex, in malesuada purus luctus sit amet. </p>
-                    <Button
-                        title={"Klikni za vec"}
-                        onClick={console.log}/>
-                </div>
+                homepage
             </div>
         )
     }
