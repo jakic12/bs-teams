37f35e146f9d9b94953dedf0ebafbe420e2cf600
Bartolomej <bartolomej.kozorog@gmail.com>
2019-07-09 16:56:58 +0200

    Test build

---
+++ build/precache-manifest.96951174c038b0a1b63e39dc2f84e99d.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e0e55961b45a43efc2676defd3a85a4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2c0e6f4e7c47f0d56b8f",
+    "url": "/static/css/main.f3664e8b.chunk.css"
+  },
+  {
+    "revision": "4e68e202ec3d89f82bc6",
+    "url": "/static/js/2.f7a86e46.chunk.js"
+  },
+  {
+    "revision": "2c0e6f4e7c47f0d56b8f",
+    "url": "/static/js/main.52a2bf5e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "06d3a43419046de017ec99af8da43838",
+    "url": "/static/media/bs_power_bel_croped.06d3a434.png"
+  },
+  {
+    "revision": "ac1e87c5d1dd6f2774f304ec2ad6f011",
+    "url": "/static/media/camera-coffe.ac1e87c5.jpg"
+  },
+  {
+    "revision": "4ecf44558a5a4828debbad12277a5bf5",
+    "url": "/static/media/drive-logo.4ecf4455.png"
+  },
+  {
+    "revision": "6323ea4d9598b13a81423aa28141e474",
+    "url": "/static/media/gmail-logo.6323ea4d.png"
+  },
+  {
+    "revision": "732879cd5bd98621f62b7976fa59e1b7",
+    "url": "/static/media/moch-feature-1.732879cd.png"
+  },
+  {
+    "revision": "90c0dae612869688c9a2d167829d72c1",
+    "url": "/static/media/moch-feature-2.90c0dae6.png"
+  },
+  {
+    "revision": "f85a1174c17ebadef7c155cb3c763859",
+    "url": "/static/media/teams-logo.f85a1174.png"
+  },
+  {
+    "revision": "527db650f42f48fed56b72cc0c2ed60a",
+    "url": "/static/media/telefon_laptop_teams_mockup.527db650.png"
+  },
+  {
+    "revision": "e19be59269719bb8fc183c947ecada6d",
+    "url": "/static/media/test-moch-image.e19be592.png"
+  },
+  {
+    "revision": "b142c6127ca08d008be133cfa2cb9dd8",
+    "url": "/static/media/word-logo.b142c612.png"
+  }
+]);
