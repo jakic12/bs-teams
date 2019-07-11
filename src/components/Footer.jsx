da26993b4230d5228fa1e7ef843ac8bd82846428
Luka <luka.gulic2@gmail.com>
2019-07-11 13:58:34 +0200

    Added Contacts and modified Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -13,13 +13,30 @@ export default class Footer extends Component{
                         </div>
                         <div className="contentRow">
                             <img src={ BsLogo } className="logo" />   
-                            <img src={ BsLogo } className="logo" />         
+                            <img src={ BsLogo } className="logo" />
                         </div>
                         <div className="contentColumn">
                             <h3>PRODUKTI</h3>
                             <p>Microsoft Teams</p>
                             <p>PowerBi</p>
                         </div>
+                    </div>
+                    <div className="copyright">
+                        <div className = "copyWrapper">
+                            <div className="copyContent">
+                                <p>&copy; 2018 Business Solution. All rights reserved.</p>
+                            </div>
+                            <div className="linkCon">
+                               <a className="link" href = "#">Privacy</a> 
+                            </div>
+                            <div className="linkCon">
+                               <a className="link" href = "#">Terms </a>
+                            </div>
+                            <div className="linkCon">
+                               <a className="link" href = "#">Cookies</a> 
+                            </div>
+
+                        </div>    
                     </div>
                 </div>
         )
