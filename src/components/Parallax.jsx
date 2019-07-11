3db3ed77cef511e1d9cd4c490600d3eeb464444f
jakob <drusany.jakob@gmail.com>
2019-07-11 14:33:36 +0200

    added test version of scroll animation

---
+++ src/components/Parallax.jsx
@@ -0,0 +1,97 @@
+import React, { Component } from 'react'
+import '../styles/parallax.scss'
+import waveCover from '../res/img/space_cloud.svg';
+import ReactSVG from 'react-svg'
+import { relative } from 'path';
+
+class Parallax extends Component{
+    constructor(props){
+        super(props)
+
+        this.sizeCoeficient = 1;
+
+        this.state = {
+            sizeCoeficient:this.sizeCoeficient,
+            parallaxEffect:true,
+            ignoreAnim:false
+        }
+    }
+    componentDidMount(){
+        //window.addEventListener('scroll', this.handleScroll);
+        var passiveIfSupported = false;
+
+        try {
+          window.addEventListener("test", null, Object.defineProperty({}, "passive", { get: function() { passiveIfSupported = { passive: true }; } }));
+        } catch(err) {}
+
+        window.addEventListener('scroll', this.handleScroll, passiveIfSupported );
+        window.addEventListener('resize', this.handleResize)
+    }
+    
+    componentWillUnmount(){
+        window.removeEventListener('scroll', this.handleScroll)
+        window.removeEventListener('resize', this.handleResize)
+    }
+
+    handleResize = () => {
+        //document.getElementById()
+    }
+    
+    handleScroll = () =>{
+        let start = 1;
+        let end = 0.2;
+        let top = document.getElementById("topParallax")
+        let wave = document.getElementById("waveCover")
+        
+        let topRect = top.getBoundingClientRect();
+        let waveRect = wave.getBoundingClientRect()
+
+        if(topRect.bottom > 0){
+            if(waveRect.top < topRect.top+waveRect.height/2){
+                
+                //if(!this.state.ignoreAnim)
+                    console.log(waveRect.top, topRect.top+waveRect.height/2)
+                    //this.setState(() => ({ignoreAnim:true, sizeCoeficient:0}))
+            }else{
+                this.sizeCoeficient = (start-end)*(topRect.bottom/(topRect.height+70))*start + end
+                window.requestAnimationFrame(() =>{
+                    this.setState(() => ({ignoreAnim:false,sizeCoeficient:this.sizeCoeficient}))
+                });
+            }
+        }
+    }
+
+    triggerAnimation = () =>{
+
+    }
+
+    render(){
+        console.log(this.state.sizeCoeficient);
+        return (
+            <React.Fragment>
+                <div id="topParallax" style={this.state.parallaxEffect?{}:{position:`relative`, background:`#3c3c3b`, height:`calc(100vh - 70px)`}}>
+                    <div className="innerParallax" style={this.state.parallaxEffect?{transform:`scale(${this.state.sizeCoeficient}) translate(0,${(1-this.state.sizeCoeficient)*-500}px)`}:{}}>
+                        {this.props.children[0]}
+                    </div>
+                    <ReactSVG 
+                        id="waveCover"
+                        src={waveCover}
+                        beforeInjection={svg => {
+                          svg.classList.add('svg-wave')
+                          svg.setAttribute('preserveAspectRatio', 'none')
+                        }}
+                        
+                        afterInjection={(err, svg)=>{
+                            svg.parentElement.setAttribute('style', '')//set wrapper style
+                        }}
+                    />
+                </div>
+                <div id="bottom_parallax">
+                    {this.props.children[1]}
+                </div>
+            </React.Fragment>
+        )
+    }
+}
+
+export default Parallax;
--- src/screens/Homepage.jsx
+++ src/screens/Homepage.jsx
@@ -30,6 +30,7 @@ import FeatureScroll from "../components/FeatureScroll"
 import BigImpact from "../components/BigImpact"
 import LeftRightContainer from "../components/LeftRightContainer"
 import FirstRow from "../components/FirstRow"
+import Parallax from "../components/Parallax"
 
 
 export default class Homepage extends Component {
@@ -73,79 +74,83 @@ export default class Homepage extends Component {
 
     render(){
         return (
-            <div style={{overflowX: "hidden"}}>
-                <FirstRow
-                    title="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
-                    description={"Nunc ac sapien vulputate odio convallis posuere nec vitae magna." +
-                    "In a efficitur ex, eget dictum elit. Nullam ac elit blandit, pharetra augue id, pulvinar ipsum." +
-                    "Maecenas vel libero odio. Mauris vehicula neque ex, in malesuada purus luctus sit amet."}
-                    btnTitle="Klikni vec"
-                    onClick="nekej"
-                    bigLogo={TelefonLaptop}
-                    smallLogo={Telefon}/>
-                <Comparison
-                    isMobile={this.props.isMobile}
-                    title={"Vsa orodja v enem"}
-                    description={"Microsoft teams zdruzuje vsa Microsoftova orodja za komunikacijo, shranjevanje in management v novo orodje ki vkljucuje vse v enem bla bla..."}
-                    animationState={this.state.comparison}
-                    leftIconsSize={100}
-                    rightIconsSize={200}
-                    teamsLogo={teamsLogo}
-                    config={[
-                        {
-                            img: mailLogo,
-                            name: "drive"
-                        },{
-                            img: wordLogo,
-                            name: "word"
-                        },{
-                            img: calendarLogo,
-                            name: "calendar"
-                        },{
-                            img: storageLogo,
-                            name: "drive"
-                        }
-                    ]}/>
-                <FeatureScroll
-                    features={[
-                        {screenshot:mochFeature1, title:"Amazing callssdsd", icon:mochFeature1logo},
-                        {screenshot:mochFeature2, title:"Cool groups", icon:mochFeature2logo},
-                        {screenshot:mochFeature1, title:"I have no idea", icon:mochFeature1logo},
-                        {screenshot:mochFeature2, title:"Works like a charm and a half", icon:mochFeature2logo},
-                        {screenshot:mochFeature1, title:"I have no idea", icon:mochFeature1logo}
-                    ]}
-                    moreLinkText={
-                        "view more features"
-                    }
-                    morePath={
-                        `#`
-                    }
-                    isMobile={this.props.isMobile}
-                />
-                <BigImpact
-                    title="Product Features"
-                    subtitle="Nekaj funkcij, ki jih je X zmozen opravljati:"
-                    content={[
-                        {title:"Feature 1", desc:"Very short description lorem ipsum dor sit amet"},
-                        {title:"Feature 2", desc:"Very short description lorem ipsum dor sit amet"},
-                        {title:"Feature 3", desc:"Very short description lorem ipsum dor sit amet"},
-                        {title:"Feature 4", desc:"Very short description lorem ipsum dor sit amet"},
-                        {title:"Feature 5", desc:"Very short description lorem ipsum dor sit amet"},
-                        {title:"Feature 6", desc:"Very short description lorem ipsum dor sit amet"}
-                    ]}
-                    image={coffeImage}
-                    buttonText={"FREE TRIAL"}
-                />
-                <LeftRightContainer
-                    title={"What is Lorem Ipsum?"}
-                    description={"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."}
-                    image={teamsImg}
-                    flip={true}/>
-                <LeftRightContainer
-                    title={"What is Lorem Ipsum?"}
-                    description={"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."}
-                    image={teamsImg}
-                    flip={false}/>
+            <div className="homepage">
+                <Parallax>
+                    <FirstRow
+                        title="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+                        description={"Nunc ac sapien vulputate odio convallis posuere nec vitae magna." +
+                        "In a efficitur ex, eget dictum elit. Nullam ac elit blandit, pharetra augue id, pulvinar ipsum." +
+                        "Maecenas vel libero odio. Mauris vehicula neque ex, in malesuada purus luctus sit amet."}
+                        btnTitle="Klikni vec"
+                        onClick={()=>{}}
+                        bigLogo={TelefonLaptop}
+                        smallLogo={Telefon}/>
+                    <div>
+                        <Comparison
+                            isMobile={this.props.isMobile}
+                            title={"Vsa orodja v enem"}
+                            description={"Microsoft teams zdruzuje vsa Microsoftova orodja za komunikacijo, shranjevanje in management v novo orodje ki vkljucuje vse v enem bla bla..."}
+                            animationState={this.state.comparison}
+                            leftIconsSize={100}
+                            rightIconsSize={200}
+                            teamsLogo={teamsLogo}
+                            config={[
+                                {
+                                    img: mailLogo,
+                                    name: "drive"
+                                },{
+                                    img: wordLogo,
+                                    name: "word"
+                                },{
+                                    img: calendarLogo,
+                                    name: "calendar"
+                                },{
+                                    img: storageLogo,
+                                    name: "drive"
+                                }
+                            ]}/>
+                        <FeatureScroll
+                            features={[
+                                {screenshot:mochFeature1, title:"Amazing callssdsd", icon:mochFeature1logo},
+                                {screenshot:mochFeature2, title:"Cool groups", icon:mochFeature2logo},
+                                {screenshot:mochFeature1, title:"I have no idea", icon:mochFeature1logo},
+                                {screenshot:mochFeature2, title:"Works like a charm and a half", icon:mochFeature2logo},
+                                {screenshot:mochFeature1, title:"I have no idea", icon:mochFeature1logo}
+                            ]}
+                            moreLinkText={
+                                "view more features"
+                            }
+                            morePath={
+                                `#`
+                            }
+                            isMobile={this.props.isMobile}
+                        />
+                        <BigImpact
+                            title="Product Features"
+                            subtitle="Nekaj funkcij, ki jih je X zmozen opravljati:"
+                            content={[
+                                {title:"Feature 1", desc:"Very short description lorem ipsum dor sit amet"},
+                                {title:"Feature 2", desc:"Very short description lorem ipsum dor sit amet"},
+                                {title:"Feature 3", desc:"Very short description lorem ipsum dor sit amet"},
+                                {title:"Feature 4", desc:"Very short description lorem ipsum dor sit amet"},
+                                {title:"Feature 5", desc:"Very short description lorem ipsum dor sit amet"},
+                                {title:"Feature 6", desc:"Very short description lorem ipsum dor sit amet"}
+                            ]}
+                            image={coffeImage}
+                            buttonText={"FREE TRIAL"}
+                        />
+                        <LeftRightContainer
+                            title={"What is Lorem Ipsum?"}
+                            description={"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."}
+                            image={teamsImg}
+                            flip={true}/>
+                        <LeftRightContainer
+                            title={"What is Lorem Ipsum?"}
+                            description={"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."}
+                            image={teamsImg}
+                            flip={false}/>
+                    </div>
+                </Parallax>
             </div>
         )
     }
