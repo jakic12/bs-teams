dbcce5abc46834dbd2280fcaafdda0763251409c
jakob <drusany.jakob@gmail.com>
2019-07-18 13:11:32 +0200

    Fixed TopBar - added burger menu

--- src/components/TopMenu.jsx
+++ src/components/TopMenu.jsx
@@ -2,23 +2,58 @@ import React, { Component } from 'react'
 import '../styles/topMenu.scss'
 import bslogo from '../res/img/bs_power_bel_croped.png'
 import { Link } from 'react-router-dom';
+import { PerspectiveX } from 'react-burgers'
 
 class TopMenu extends Component{
+    constructor(props){
+        super(props)
+
+        this.state = {
+            menuOpen:false
+        }
+    }
     render(){
+        const logo = <div className="topBarBlock">
+            <img className="bslogo" src={bslogo} alt="Business Solutions logo"/>
+        </div>
+        const blocks = <React.Fragment>
+            <div className="topBarBlock">
+                <div className="topLinkWrapper">
+                    <Link onClick={() => {this.setState({menuOpen:false})}} className="topLink" to="/">Domov</Link>
+                </div>
+            </div>
+        </React.Fragment>
+
         return (
-            <div className="topBar" style={this.props.style}>
+            <div className={`topBar ${(this.state.menuOpen && this.props.isMobile) && `menuOpen`}`} style={this.props.style}>
                 <div className="innerBarWrapper">
                     <div className="innerBar">
-                        <div className="topBarBlock">
-                            <img className="bslogo" src={bslogo} alt="Business Solutions logo"/>
-                        </div>
-                        <div className="topBarBlock">
-                            <div className="topLinkWrapper">
-                                <Link className="topLink" to="/">Domov</Link>
+                        {this.props.isMobile? <React.Fragment>
+                            <div className="logo">
+                                {logo}
                             </div>
-                        </div>
+                            <div classname="topBarButton">
+                                <PerspectiveX
+                                    color='#fff'
+                                    active={this.state.menuOpen}
+                                    onClick={() => {
+                                        this.setState(({ menuOpen }) => ({ menuOpen: !menuOpen }))
+                                    }}
+                                />
+                            </div>
+                        </React.Fragment> : [logo, blocks]}
                     </div>
                 </div>
+                
+                        {this.props.isMobile &&
+                        <div className={`blocks`}>
+                            <div className="innerBarWrapper">
+                                <div className="innerBar">
+                                    {blocks}
+                                </div>
+                            </div>
+                        </div>}
+                    
             </div>
         )
     }
