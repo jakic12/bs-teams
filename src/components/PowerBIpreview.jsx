b10d85b81cea2a3d36614159fa544000df4a9889
jakob <drusany.jakob@gmail.com>
2019-07-19 10:07:22 +0200

    Changed powe Bi preview link

--- src/components/PowerBIpreview.jsx
+++ src/components/PowerBIpreview.jsx
@@ -9,7 +9,7 @@ export default ({title, description, btnTitle, onClick, bigLogo, smallLogo, styl
     const sizeY = window.innerHeight-paddingY;
     return (
         <div id="powerBIpreview" className="PowerBIpreview" style={{...style, width:`100vw`, height: sizeY + paddingY/2, padding:`${paddingY/2}px ${paddingX/2}px`}}>
-            {(display) && <iframe width={sizeX} height={sizeY} src="https://app.powerbi.com/view?r=eyJrIjoiMWQzYWVmOWEtMGY3Ni00MDIwLTk3M2ItMGRjOGJjNWQyNGM5IiwidCI6IjEyNGQyYzU3LTQ1N2EtNDNhNy1iNjJiLTgzODA5ODUzZjdjOSIsImMiOjh9" frameborder="0" allowFullScreen="true"></iframe>}
+            {(display) && <iframe width={sizeX} height={sizeY} src="https://app.powerbi.com/view?r=eyJrIjoiZTYzOWEzZjktOGNiMS00MTFmLWFlMmQtYmI5MGUxOGJmMDQ1IiwidCI6IjEyNGQyYzU3LTQ1N2EtNDNhNy1iNjJiLTgzODA5ODUzZjdjOSIsImMiOjh9" frameborder="0" allowFullScreen="true"></iframe>}
         </div>
     )
 }
