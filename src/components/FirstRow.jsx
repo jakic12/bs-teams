a147358b0886c127877e4242b388617aefc017a7
jakob <drusany.jakob@gmail.com>
2019-07-19 09:11:18 +0200

    Styles fixes

--- src/components/FirstRow.jsx
+++ src/components/FirstRow.jsx
@@ -6,20 +6,20 @@ import Button from '../components/Button'
 
 export default ({title, description, btnTitle, onClick, bigLogo, smallLogo, style}) => {
     return (
-        <div className="firstRow" style={style}>
-            <div className="left">
-                <div className="leftInner">
-                    <h1 className={'bigLightTitle'}>{title}</h1>
-                    <p className={'lightParagraph'}>{description}</p>
-                    <div className="btnWraper">
-                        <Button
-                            title={btnTitle}
-                            onClick={onClick}/>
-                    </div>
+        <div className="homeLandingViewContainer" style={style}>
+            <div className="innerTextContainer">
+                <h1 className={'btMargin bigLightTitle'}>{title}</h1>
+                <h3 className={'btMargin mediumLightTitle'}>{subtitle}</h3>
+                <p className={'btMargin lightParagraph'}>{description}</p>
+                <div className="btnWrapper">
+                    <Button
+                        title={btnTitle}
+                        onClick={onClick}/>
                 </div>
             </div>
-            <div className="right">
-                <img src={bigLogo} className="telefonLaptop" />
+            <div className="innerImageContainer">
+              <img src={bigLogo} className="bigLogo"/>
+              <img src={smallLogo} className="smallLogo"/>
             </div>
         </div>
     )
