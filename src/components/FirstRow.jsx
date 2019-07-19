881c38a1a34dbdadefda1368b5213f4c2d8c9951
jakob <drusany.jakob@gmail.com>
2019-07-19 09:19:53 +0200

    small layout imporvements

--- src/components/FirstRow.jsx
+++ src/components/FirstRow.jsx
@@ -8,9 +8,9 @@ export default ({title, description, btnTitle, onClick, bigLogo, smallLogo, styl
     return (
         <div className="homeLandingViewContainer" style={style}>
             <div className="innerTextContainer">
-                <h1 className={'btMargin bigLightTitle'}>{title}</h1>
-                <h3 className={'btMargin mediumLightTitle'}>{subtitle}</h3>
-                <p className={'btMargin lightParagraph'}>{description}</p>
+                <h1 className={'bigText bigLightTitle'}>{title}</h1>
+                <h3 className={'mediumText mediumLightTitle'}>{subtitle}</h3>
+                <p className={'smallText lightParagraph'}>{description}</p>
                 <div className="btnWrapper">
                     <Button
                         title={btnTitle}
